a31aa9e4759f630068a29be786c635c069f0cf44
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-09-14 20:49:10 +0930

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { ReactionsPage } from '../pages/reactions/reactions';
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ReactionsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ReactionsPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, PopoverController } from 'ionic-angular';
+import { ReactionsPage } from '../../pages/reactions/reactions';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, private popoverCtrl: PopoverController) {
+		// THIS IS JUST FOR TESTING
+		window.addEventListener("contextmenu", (e) => { e.preventDefault(); });
+	}
 
-  }
+	showReactions(ev){
+
+		let reactions = this.popoverCtrl.create(ReactionsPage);
+
+		reactions.present({
+			ev: ev
+		});
+
+	}
+
+	like(){
+		console.log("like");
+	}
 
 }
